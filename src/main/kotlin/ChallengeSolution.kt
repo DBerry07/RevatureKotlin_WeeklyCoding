4ecbccdafca1d0fb357d5753b2d753300672b65d
DBerry07 <dwjberry07@gmail.com>
2022-04-01 10:21:42 -0400

    Created abstract class ChallengeSolution

---
+++ src/main/kotlin/ChallengeSolution.kt
@@ -0,0 +1,15 @@
+abstract class ChallengeSolution {
+
+    public abstract fun solve();
+
+    public fun println(str : String) : Boolean {
+        System.out.println(str)
+        return true
+    }
+
+    public fun println(int : Int) : Boolean{
+        System.out.println(int)
+        return true
+    }
+
+}
