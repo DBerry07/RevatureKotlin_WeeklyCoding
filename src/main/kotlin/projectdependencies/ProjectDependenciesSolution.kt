9e7b0e7942e4a3279d46f268d78a3e2f5baf4ed7
DBerry07 <dwjberry07@gmail.com>
2022-04-18 16:26:38 -0400

    Added methods to ProjectDependenciesSolution
    Added more methods to ProjectDependenciesSolution (CountInstances and findDependents) and changed said class to an object

--- src/main/kotlin/projectdependencies/ProjectDependenciesSolution.kt
+++ src/main/kotlin/projectdependencies/ProjectDependenciesSolution.kt
@@ -2,20 +2,53 @@ package projectdependencies
 
 import core.ChallengeSolution
 
-class ProjectDependenciesSolution : ChallengeSolution() {
+object ProjectDependenciesSolution : ChallengeSolution() {
 
     public override fun solve() {
-        var projects = arrayOf('a', 'b', 'c', 'd', 'e', 'f')
-        var dependencies : Array<Set<Char>> = arrayOf(
+        var output = String()
+        val projects = arrayOf('a', 'b', 'c', 'd', 'e', 'f')
+        val dependencies : Array<Set<Char>> = arrayOf(
             setOf('a', 'd'), setOf('f', 'b'), setOf('b', 'd'), setOf('f', 'a'), setOf('d', 'c')
         )
+
+        var hashMap = createHashMap(projects)
+        hashMap = countInstances(dependencies, hashMap)
+
+        //projects.indices rather than "i in 0..projects.size - 1"
+        for (i in projects.indices) {
+            if (hashMap[i] == 0) {
+                //'a'.code rather than 'a'.toInt()
+                output = output + (i + 'a'.code).toChar() +", "
+            }
+        }
     }
 
-    private fun createHashMap(array : Array<Char>) : HashMap<Char, Int> {
-        var hashMap = HashMap<Char, Int>()
+    private fun createHashMap(array : Array<Char>) : HashMap<Int, Int> {
+        val hashMap = HashMap<Int, Int>()
         for (each in array) {
-            hashMap[each] = 0
+            hashMap[each.toInt()] = 0
         }
         return hashMap
+    }
+
+    private fun countInstances(dependencies: Array<Set<Char>>, hashMap: HashMap<Int, Int>): HashMap<Int, Int> {
+        for (set in dependencies) {
+            val element: Int = set.elementAt(1).code - 'a'.code
+            hashMap[element] = hashMap[element]!! + 1
+        }
+        return hashMap
+    }
+
+    private fun findDependents(main : Char, dependencies: Array<Set<Char>>, string: String) {
+        for (set in dependencies) {
+            var newString = string
+            val pend = set.elementAt(1)
+            val dant = set.elementAt(0)
+
+            if (dant.equals(main)) {
+                newString = "$newString$dant, "
+            }
+        }
+        
     }
 }
