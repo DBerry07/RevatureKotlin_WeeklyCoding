ed77e4449c11fab55326e422b54edbf2f1b90365
DBerry07 <dwjberry07@gmail.com>
2022-05-09 14:18:51 -0400

    Added "magic" package

--- src/main/kotlin/listandloop/ListAndLoopSolution.kt
+++ src/main/kotlin/listandloop/ListAndLoopSolution.kt
@@ -1,10 +1,21 @@
 package listandloop
 
 import core.ChallengeSolution
+import java.util.LinkedList
 
 object ListAndLoopSolution: ChallengeSolution() {
+
+    val linkedListOne: LinkedList<Int> = LinkedList<Int>()
+    val linkedListTwo: LinkedList<Int> = LinkedList<Int>()
+
     override fun solve() {
         TODO("Not yet implemented")
+
+
+    }
+
+    fun initializeInputs() {
+
     }
 
 }
